b65ca2b695254965d1f02ffca8756705da3719e7
Samarth Bhadane <samarth9201>
2020-08-06 16:35:51 +0530

    Some Source Files Added

---
+++ src/chainConfig.h
@@ -0,0 +1,26 @@
+#ifndef _CHAIN_CONFIG_H_
+#define _CHAIN_CONFIG_H
+
+
+#include <stdint.h>
+#include "os.h"
+
+typedef enum chain_kind_e{
+
+    CHAIN_KIND_XINFIN,
+    CHAIN_KIND_APOTHEM
+} chain_kind_t;
+
+typedef struct chain_config_s {
+	const char* coinName; // ticker
+	uint32_t chainId;
+	chain_kind_t kind;
+#ifdef TARGET_BLUE
+    const char* header_text;
+    unsigned int color_header;
+    unsigned int color_dashboard;
+#endif // TARGET_BLUE
+
+} chain_config_t;
+
+#endif
---
+++ src_common/Utils.h
@@ -0,0 +1,39 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include <stdint.h>
+
+#include "cx.h"
+
+bool rlpDecodeLength(uint8_t *buffer, uint32_t bufferLength, uint32_t *fieldLength, uint32_t *offset, bool * list);
+
+bool rlpCanDecode(uint8_t *buffer, uint32_t bufferLength, bool *valid);
+
+void getAddressFromKey(cx_ecfp_public_key_t *publicKey, uint8_t *out, cx_sha3_t *sha3Context);
+
+void getAddressStringFromKey(cx_ecfp_public_key_t *publicKey, uint8_t *out, cx_sha3_t *sha3Context);
+
+void getAddressStringFromBinary(uint8_t *address, uint8_t *out, cx_sha3_t *sha3Context);
+
+ool adjustDecimals(char *src, uint32_t srcLength, char *target,
+                    uint32_t targetLength, uint8_t decimals);
+
+inline int allzeroes(uint8_t *buf, int n) {
+  for (int i = 0; i < n; ++i) {
+    if (buf[i]) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+inline int ismaxint(uint8_t *buf, int n) {
+  for (int i = 0; i < n; ++i) {
+    if (buf[i] != 0xff) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+#endif
