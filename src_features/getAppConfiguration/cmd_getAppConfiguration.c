eabdd9122c5b6d0d9f9f66e2925de40b64ebf3f9
Samarth Bhadane <samarth9201>
2020-09-08 10:43:30 +0530

    Add getAppConfiguration

---
+++ src_features/getAppConfiguration/cmd_getAppConfiguration.c
@@ -0,0 +1,24 @@
+#include "shared_context.h"
+#include "apdu_constants.h"
+#ifdef TARGET_BLUE
+#include "ui_blue.h"
+#endif
+#ifdef HAVE_UX_FLOW
+#include "ui_flow.h"
+#endif
+
+void handleGetAppConfiguration(uint8_t p1, uint8_t p2, uint8_t *workBuffer, uint16_t dataLength, unsigned int *flags, unsigned int *tx) {
+  UNUSED(p1);
+  UNUSED(p2);
+  UNUSED(workBuffer);
+  UNUSED(dataLength);
+  UNUSED(flags);
+  G_io_apdu_buffer[0] = (N_storage.dataAllowed ? APP_FLAG_DATA_ALLOWED : 0x00);
+
+  G_io_apdu_buffer[1] = LEDGER_MAJOR_VERSION;
+  G_io_apdu_buffer[2] = LEDGER_MINOR_VERSION;
+  G_io_apdu_buffer[3] = LEDGER_PATCH_VERSION;
+  *tx = 4;
+  THROW(0x9000);
+}
+
